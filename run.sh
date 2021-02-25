9fc93fcd320ddab613ef93785e00c69cb05df92c
JustDean <thedetectivl@gmail.com>
2021-02-25 20:06:01 +0300

    updated run.sh dateutil version

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cat app/config_prod.yaml | envsubst > app/config.yaml
 export PYTHONPATH=$PYTHONPATH:.
-pip install python-dateutil==2.81
+pip install python-dateutil==2.8.1
 alembic upgrade head
 python main.py
